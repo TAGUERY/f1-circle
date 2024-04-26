e9083c929d560110194665f94e909acc9652cbe5
morgui2003 <devalsmorgane@gmail.com>
2024-04-26 13:10:03 +0200

    ajout texte cercles

--- src/morgane/circles-section.js
+++ src/morgane/circles-section.js
@@ -1,5 +1,14 @@
-import pilotes from "/data/pilotes.json";
 import anecdotes from "/data/anecdotes.json";
+
+let chiffre1 = anecdotes.anecdotes[5].chiffre;
+let chiffre2 = anecdotes.anecdotes[6].chiffre;
+let chiffre3 = anecdotes.anecdotes[7].chiffre;
+let chiffre4 = anecdotes.anecdotes[8].chiffre;
+
+let reponse1 = anecdotes.anecdotes[5].reponse;
+let reponse2 = anecdotes.anecdotes[6].reponse;
+let reponse3 = anecdotes.anecdotes[7].reponse;
+let reponse4 = anecdotes.anecdotes[8].reponse;
 
 function createFourCircle() {
   // Obtenir la largeur et la hauteur de la fenêtre
@@ -16,45 +25,99 @@ function createFourCircle() {
     .attr("width", width)
     .attr("height", height);
 
-  // Récupérer les anecdotes spécifiques en fonction de leurs clés
-  let pilotesAnecdote = anecdotes.anecdotes.find(
-    (anecdote) => anecdote.chiffre === "1004"
-  );
-  let viewersAnecdote = anecdotes.anecdotes.find(
-    (anecdote) => anecdote.chiffre === "90 millions"
-  );
-  let timeAnecdote = anecdotes.anecdotes.find(
-    (anecdote) => anecdote.chiffre === "10.6 seconds"
-  );
-  let priceAnecdote = anecdotes.anecdotes.find(
-    (anecdote) => anecdote.chiffre === "12 millions dollars"
-  );
+  //cercle 1, en haut à gauche
+  let circle1 = svg;
+  circle1
+    .append("circle")
+    .attr("cx", width / 2 - radius / 1.3)
+    .attr("cy", height / 4)
+    .attr("r", radius / 1.5)
+    .style("fill", "red");
 
-  // Afficher le texte dans chaque cercle avec les anecdotes spécifiques
-  let circlesData = [
-    { anecdote: pilotesAnecdote, x: width / 2 - radius / 1.3, y: height / 4 },
-    { anecdote: viewersAnecdote, x: width / 2 + radius / 1.3, y: height / 4 },
-    { anecdote: timeAnecdote, x: width / 2 - radius / 1.3, y: (3 * height) / 4 },
-    { anecdote: priceAnecdote, x: width / 2 + radius / 1.3, y: (3 * height) / 4 }
-  ];
+  circle1
+    .append("text")
+    .text(reponse1)
+    .attr("class", "reponse")
+    .attr("x", width / 2 - radius / 1.3)
+    .attr("y", height / 4 + 20);
 
-  circlesData.forEach((circleData) => {
-    let circle = svg
-      .append("circle")
-      .attr("cx", circleData.x)
-      .attr("cy", circleData.y)
-      .attr("r", radius / 1.5)
-      .style("fill", "red");
+  circle1
+    .append("text")
+    .text(chiffre1)
+    .attr("class", "chiffre")
+    .attr("x", width / 2 - radius / 1.3)
+    .attr("y", height / 4);
 
-    circle
-      .append("text")
-      .text(circleData.anecdotes.chiffre + " - " + circleData.anecdotes.reponse)
-      .style("fill", "white")
-      .style("font-size", "1.5rem")
-      .style("text-anchor", "middle")
-      .attr("x", circleData.x)
-      .attr("y", circleData.y);
-  });
+  //cercle 2, en haut à droite
+  // Cercle 2, en haut à droite
+  let circle2 = svg;
+  circle2
+    .append("circle")
+    .attr("cx", width / 2 + radius / 1.3)
+    .attr("cy", height / 4)
+    .attr("r", radius / 1.5)
+    .style("fill", "red");
+
+  circle2
+    .append("text")
+    .text(reponse2)
+    .attr("class", "reponse")
+    .attr("x", width / 2 + radius / 1.3)
+    .attr("y", height / 4 + 20);
+
+  circle2
+    .append("text")
+    .text(chiffre2)
+    .attr("class", "chiffre")
+    .attr("x", width / 2 + radius / 1.3)
+    .attr("y", height / 4);
+
+  // Cercle 3, en bas à gauche
+  let circle3 = svg;
+  circle3
+    .append("circle")
+    .attr("cx", width / 2 - radius / 1.3)
+    .attr("cy", (3 * height) / 4)
+    .attr("r", radius / 1.5)
+    .style("fill", "red");
+
+  circle3
+    .append("text")
+    .text(reponse3)
+    .attr("class", "reponse")
+    .attr("x", width / 2 - radius / 1.3)
+    .attr("y", (3 * height) / 4 + 20);
+
+  circle3
+    .append("text")
+    .text(chiffre3)
+    .attr("class", "chiffre")
+    .attr("x", width / 2 - radius / 1.3)
+    .attr("y", (3 * height) / 4);
+
+  // Cercle 4, en bas à droite
+  let circle4 = svg;
+  circle4
+    .append("circle")
+    .attr("cx", width / 2 + radius / 1.3)
+    .attr("cy", (3 * height) / 4)
+    .attr("r", radius / 1.5)
+    .style("fill", "red");
+
+  circle4
+    .append("text")
+    .text(reponse4)
+    .attr("class", "reponse")
+    .attr("x", width / 2 + radius / 1.3)
+    .attr("y", (3 * height) / 4 + 20);
+
+  circle4
+    .append("text")
+    .text(chiffre4)
+    .attr("class", "chiffre")
+    .attr("x", width / 2 + radius / 1.3)
+    .attr("y", (3 * height) / 4);
+
 }
 
 export { createFourCircle };
