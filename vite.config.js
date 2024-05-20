b457dd2e55f692bd07d5e7f265828872dd6a55d6
Tristan Montanari <tristan.montanari@heig-vd.ch>
2024-05-20 21:56:23 +0200

    essai pour fix

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,6 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
+  base: "/", // Spécifiez le chemin de base approprié
   plugins: [vue()],
-  base: "/",
-  build: {
-    outDir: "dist",
-    rollupOptions: {
-      output: {
-        assetFileNames: (assetInfo) => {
-          if (/\.(gif|jpe?g|png|svg)$/.test(assetInfo.name)) {
-            return "assets/images/[name]-[hash][extname]";
-          }
-          return "assets/[name]-[hash][extname]";
-        },
-      },
-    },
-  },
 });
